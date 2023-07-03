35c17b63285cf67fc6b58e1623451939e3629198
Garaev Maksim <garaev.maxim2013@yandex.ru>
2023-07-03 16:26:04 +0300

    fixture

--- test_lesson_3_2_step13.py
+++ test_lesson_3_2_step13.py
@@ -7,18 +7,18 @@ import time
 class TestForm(unittest.TestCase):
     def test_1(self):
     
-        link = "http://suninjuly.github.io/registration2.html"
+        link = "http://suninjuly.github.io/registration1.html"
         browser = webdriver.Chrome()
         browser.get(link)
 
         # Ваш код, который заполняет обязательные поля
-        input_first_name = browser.find_element(By.CSS_SELECTOR, 'input.first:required')
+        input_first_name = browser.find_element(By.CSS_SELECTOR, 'input.first')
         input_first_name.send_keys('Ivan')
 
-        input_last_name = browser.find_element(By.CSS_SELECTOR, 'input.second:required')
+        input_last_name = browser.find_element(By.CSS_SELECTOR, 'input.second')
         input_last_name.send_keys('Petrov')
 
-        input_email = browser.find_element(By.CSS_SELECTOR, 'input.third:required')
+        input_email = browser.find_element(By.CSS_SELECTOR, 'input.third')
         input_email.send_keys('123@mail.ru')
 
 
@@ -40,8 +40,46 @@ class TestForm(unittest.TestCase):
         # закрываем браузер после всех манипуляций
         browser.quit()
         
-        self.assertEqual("Congratulations! You have successfully registered!" == welcome_text)
+        self.assertEqual("Congratulations! You have successfully registered!" == welcome_text,True)
 
+    
+    def test_2(self):
+    
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        input_first_name = browser.find_element(By.CSS_SELECTOR, 'input.first')
+        input_first_name.send_keys('Ivan')
+
+        input_last_name = browser.find_element(By.CSS_SELECTOR, 'input.second')
+        input_last_name.send_keys('Petrov')
+
+        input_email = browser.find_element(By.CSS_SELECTOR, 'input.third')
+        input_email.send_keys('123@mail.ru')
+
+        
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.XPATH, '//button[@type="submit"]')
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+        
+        self.assertEqual("Congratulations! You have successfully registered!" == welcome_text,False)
 
 
 if __name__ == '__main__':
